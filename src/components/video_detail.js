b4bc5ba3085b66b59dcca02c326ef32f8f18e97d
thomas159 <thomas159@gmail.com>
2017-02-17 15:51:34 +0100

    initial commit

---
+++ src/components/video_detail.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const VideoDetail = ({ video }) => {
+    if (!video) {
+        console.log("1")
+        console.log({video})
+        return (<div>Loading...</div>);
+    }
+    console.log("2")
+    console.log({video})
+    console.log( video.id.videoId)
+
+    const videoId = video.id.videoId;
+    const url = `https://www.youtube.com/embed/v/${videoId}`;
+
+    return (
+        <div className="video-detail col-lg-16">
+            <div className="embed-responsive embed-responsive-16by9">
+                <iframe className="embed-responsive-item" src={url}></iframe>
+            </div>
+            <div className="details">
+                <div>{video.snippet.title}</div>
+                <div>{video.snippet.description}</div>
+            </div>
+        </div>
+    );
+
+};
+
+export default VideoDetail;
---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyDJb4e_IuNBUP7znSed_837TvLwtEv17EY'; //Youtube API KEY
+
+
+
+class App extends Component {
+
+    constructor(props) { 
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+        this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail videos={this.state.videos[0]} />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+}    
+// Take this component's generated HTML and put it on the page 
+// (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
